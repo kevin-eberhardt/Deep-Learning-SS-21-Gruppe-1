70fcb1da967539eefe86bb58d13b0a8c115dd9be
kevin-eberhardt <kevin96e@live.de>
2021-06-22 17:59:32 +0200

    pip freeze only necessary modules

--- yolov3/yolo_task_2b.py
+++ yolov3/yolo_task_2b.py
@@ -12,4 +12,4 @@ complete_path = "/Users/kevineberhardt/Coding/Privates/Deep-Learning-SS-21-Grupp
 yolo = Create_Yolo(input_size=YOLO_INPUT_SIZE, CLASSES=TRAIN_CLASSES)
 yolo.load_weights("./trained_model/yolov3_custom_v2_half_data")
 #detect_video(yolo, video_path = file_path, output_path = output_path + video_file_name, score_threshold=0.32, iou_threshold=0.32, CLASSES=TRAIN_CLASSES)
-yolo3_detect_video_2a(video_path = complete_path, output_dir=output_path, score_threshold=0.32, iou_threshold=0.32,draw_roi=False, zoom=0.3,show_diver=True)
+yolo3_detect_video_2a(video_path = complete_path, output_dir=output_path, score_threshold=0.32, iou_threshold=0.32,draw_roi=False, zoom=0.3)
