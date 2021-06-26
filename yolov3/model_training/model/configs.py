02f739acd389a5a0a7a47700a175e7cebf8346c3
JuliusJacobitz <julius@jacobitz.de>
2021-06-26 16:14:58 +0200

    abgabe part 2b

--- yolov3/model_training/model/configs.py
+++ yolov3/model_training/model/configs.py
@@ -1,10 +1,10 @@
 YOLO_TYPE                   = "yolov3" # yolov4 or yolov3
 YOLO_FRAMEWORK              = "tf" # "tf" or "trt"
-YOLO_V3_WEIGHTS             = "./model_training/model_data/yolov3.weights"
+YOLO_V3_WEIGHTS             = "yolov3/model_training/model_data/yolov3.weights"
 YOLO_TRT_QUANTIZE_MODE      = "INT8" # INT8, FP16, FP32
 YOLO_CUSTOM_WEIGHTS         = False # "checkpoints/yolov3_custom" # used in evaluate_mAP.py and custom model detection, if not using leave False
                             # YOLO_CUSTOM_WEIGHTS also used with TensorRT and custom model detection
-YOLO_COCO_CLASSES           = "./model_training/model_data/coco/coco.names"
+YOLO_COCO_CLASSES           = "yolov3/model_training/model_data/coco/coco.names"
 YOLO_STRIDES                = [8, 16, 32]
 YOLO_IOU_LOSS_THRESH        = 0.5
 YOLO_ANCHOR_PER_SCALE       = 3
@@ -19,9 +19,9 @@ TRAIN_YOLO_TINY             = False
 TRAIN_SAVE_BEST_ONLY        = True # saves only best model according validation loss (True recommended)
 TRAIN_SAVE_CHECKPOINT       = False # saves all best validated checkpoints in training process (may require a lot disk space) (False recommended)
 #TRAIN_CLASSES               = "mnist/mnist.names"
-TRAIN_CLASSES               = "./model_training/model_data/class_names.txt"
+TRAIN_CLASSES               = "yolov3/model_training/model_data/class_names.txt"
 #TRAIN_ANNOT_PATH            = "mnist/mnist_train.txt"
-TRAIN_ANNOT_PATH            = "./model_training/model_data/class_train.txt"
+TRAIN_ANNOT_PATH            = "yolov3/model_training/model_data/class_train.txt"
 
 TRAIN_LOGDIR                = "log"
 TRAIN_CHECKPOINTS_FOLDER    = "checkpoints"
@@ -39,7 +39,7 @@ TRAIN_EPOCHS                = 100
 
 # TEST options
 #TEST_ANNOT_PATH             = "mnist/mnist_test.txt"
-TEST_ANNOT_PATH             = "./model_training/model_data/class_test.txt"
+TEST_ANNOT_PATH             = "yolov3/model_training/model_data/class_test.txt"
 TEST_BATCH_SIZE             = 4
 TEST_INPUT_SIZE             = 416
 TEST_DATA_AUG               = False
