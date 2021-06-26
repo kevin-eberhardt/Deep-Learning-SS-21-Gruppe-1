37d3f2a564814f79524d62bdbab5d08168697b40
Jonas Götz <jg133@hdm-stuttgart.de>
2021-06-26 14:57:26 +0200

    updated knn

--- Abgabe/Task 2a/Beste Modelle/KNN.py
+++ Abgabe/Task 2a/Beste Modelle/KNN.py
@@ -1,8 +1,7 @@
 import cv2 as cv
-import numpy as np
-from time import sleep
 
-VIDEOPATH = 'C:\\Users\\goetz\\Desktop\\DL Videos\\_tigfCJFLZg_00314.mp4'
+
+VIDEOPATH = 'C:\\Users\\goetz\\Desktop\\_tigfCJFLZg_00187.mp4'
 
 kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (3, 3)) # Größe der Pixel: 1 default -> keine noise reduction
 
@@ -22,10 +21,9 @@ while True:
     if frame is None:
         break
 
-    fgMask = backSub.apply(frame)#learningRate=0.4
+    fgMask = backSub.apply(frame)
     fgMask = cv.morphologyEx(fgMask, cv.MORPH_OPEN, kernel)
 
-    sleep(0.1)
     cv.imshow('Original', frame)
     cv.imshow('KNN best result', fgMask)
     keyboard = cv.waitKey(30)
